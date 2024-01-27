f8a1fbb35653fcf7d25c525de8275d2112f42f43
Daniel Kleyman <denny_klm1@yahoo.com>
2024-01-27 19:14:34 +0200

    HW-66

---
+++ drones-medications/src/main/java/telran/drones/dto/MedicationDto.java
@@ -0,0 +1,5 @@
+package telran.drones.dto;
+
+public record MedicationDto(String code, String name, int weight) {
+
+}
--- drones-medications/src/main/java/telran/drones/model/Medication.java
+++ drones-medications/src/main/java/telran/drones/model/Medication.java
@@ -1,5 +1,33 @@
 package telran.drones.model;
 //TODO create entity Medication
-public class Medication {
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import telran.drones.dto.MedicationDto;
+import telran.drones.dto.State;
+
+@Entity
+@Table(name = "medications")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Medication {
+	@Id
+	@Column(name = "medication_code", nullable = false)
+	String code;
+	@Column(name = "medication_name", nullable = false)
+	String name;
+	@Column(name = "medication_weight", nullable = false)
+	int weight;
+
+	static public Medication of(MedicationDto medicationDto) {
+		return new Medication(medicationDto.code(), medicationDto.name(), medicationDto.weight());
+	}
+
+	public MedicationDto build() {
+		return new MedicationDto(code, name, weight);
+	}
 }
