6f7485a328eee6b24e400dadd87dcfb7135df877
Daniel Kleyman <denny_klm1@yahoo.com>
2024-01-27 18:40:29 +0200

    HW-66

---
+++ drones-medications/src/main/java/telran/spring/DronesMedicationsApplication.java
@@ -0,0 +1,13 @@
+package telran.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesMedicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronesMedicationsApplication.class, args);
+	}
+
+}
